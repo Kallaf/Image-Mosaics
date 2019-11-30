43437bd784b229f975691b3ed8ccab582b74aff0
Youssef Sherif <youssefsabry97@gmail.com>
2019-11-30 22:59:42 +0200

    Merge branch sift into master

--- CorrespondencesGetter.py
+++ CorrespondencesGetter.py
@@ -1,9 +1,23 @@
 import matplotlib.pyplot as plt
 from matplotlib.patches import Circle
+import matplotlib.image as mpimg
 import numpy as np
 import cv2
 import random
 
+def getCorrespondences(img1_url,img2_url,n):
+    img=mpimg.imread(img1_url)
+    imgplot = plt.imshow(img)
+    print("Please select %d points" % n)
+    x = plt.ginput(n)
+    plt.show()
+
+    img=mpimg.imread(img2_url)
+    imgplot = plt.imshow(img)
+    print("Please select %d points correspondence the points selected from image 1" % n)
+    y = plt.ginput(n)
+    plt.show()
+    return x,y
 
 def getCorrespondences_auto(img1_url, img2_url):
 
